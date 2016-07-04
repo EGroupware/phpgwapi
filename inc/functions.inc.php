d0d93ff50d22788e9df4a2dae0ff049d4dcd354d
Ralf Becker <RalfBecker@outdoor-training.de>
2016-07-04 16:35:53 +0200

    * Api/Setup: show new version 16.1.002 even if old header.inc.php is used

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -10,4 +10,8 @@
  * @version $Id$
  */
 
+// fix APi version shown with old header.inc.php
+include(EGW_SERVER_ROOT.'/api/setup/setup.inc.php');
+$GLOBALS['egw_info']['server']['versions']['phpgwapi'] = $GLOBALS['egw_info']['server']['versions']['api'] = $setup_info['api']['version'];
+
 require_once dirname(dirname(__DIR__)).'/api/src/loader.php';
